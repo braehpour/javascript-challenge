dd1171e338a3aeae8bfd547a18d28e4699486cd2
braehpour <braehpour@gmail.com>
2020-11-04 17:25:04 -0600

    completed filtering but having issue with default

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,6 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
 var tbody = d3.select('tbody');
 
 tableData.forEach(function(sighting){
@@ -13,10 +11,35 @@ tableData.forEach(function(sighting){
     })
 })
 
+var form = d3.select('.form-group')
 var filterButton = d3.select('#filter-btn');
 
-filterButton.on('click', filterClick)
+filterButton.on('click', filterClick);
+form.on('submit', filterClick);
 
 function filterClick() {
-    console.log(d3.event.target)
+    
+    d3.event.preventDefault();
+    
+    var input = d3.select('#datetime');
+    var inputValue = input.property('value');
+    console.log(inputValue)
+
+    var filteredData = tableData.filter(function(ufo) {
+        return ufo.datetime == inputValue
+    });
+
+    console.log(filteredData)
+
+    var summary = d3.select('#ufo-table');
+
+    d3.selectAll('tr').remove();
+
+    filteredData.forEach(function(ufo) {
+        var currentRow = tbody.append('tr');
+    
+        Object.entries(ufo).forEach(function([key, value]){
+            currentRow.append('td').text(value)
+        })
+    })
 }
