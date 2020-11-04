06456f8d429cc23c84a7a9ec741f6c5c970ae769
braehpour <braehpour@gmail.com>
2020-11-04 15:23:59 -0600

    added table data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select('tbody');
+
+tableData.forEach(function(sighting){
+    var currentRow = tbody.append('tr');
+
+    Object.entries(sighting).forEach(function([key, value]){
+        console.log(key, value)
+        currentRow.append('td').text(value)
+    })
+})
